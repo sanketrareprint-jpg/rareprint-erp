d4706c456b176deccda7cbfce7dbdc83a91228bf
Sanket <sanket.rareprint@gmail.com>
2026-04-17 16:35:13 +0530

    fix: AccountsModule imports WhatsAppModule

--- backend/src/accounts/accounts.module.ts
+++ backend/src/accounts/accounts.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AccountsController } from './accounts.controller';
 import { AccountsService } from './accounts.service';
+import { AccountsController } from './accounts.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { WhatsAppModule } from '../whatsapp/whatsapp.module';
 
 @Module({
+  imports: [PrismaModule, WhatsAppModule],
   controllers: [AccountsController],
   providers: [AccountsService],
 })
