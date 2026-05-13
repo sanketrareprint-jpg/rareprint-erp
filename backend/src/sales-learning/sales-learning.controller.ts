ae3c2e7b30a2ebeb16ae7d891b18d96449c211fe
Sanket Rareprint <your@email.com>
2026-05-13 14:46:48 +0530

    fix: use req.user.id in sales-learning controller

--- backend/src/sales-learning/sales-learning.controller.ts
+++ backend/src/sales-learning/sales-learning.controller.ts
@@ -1,4 +1,4 @@
-﻿import { Controller, Get, Post, Body, Param, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UseGuards, Request } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { SalesLearningService } from './sales-learning.service';
 
@@ -9,16 +9,16 @@ export class SalesLearningController {
 
   @Get('topics')
   getTopics(@Request() req) {
-    return this.salesLearningService.getTopicsForUser(req.user.userId);
+    return this.salesLearningService.getTopicsForUser(req.user.id);
   }
 
   @Post('topics/:id/complete')
   completeTopic(@Param('id') id: string, @Request() req, @Body() body: any) {
-    return this.salesLearningService.completeTopic(req.user.userId, id, body.score, body.totalQuestions);
+    return this.salesLearningService.completeTopic(req.user.id, id, body.score, body.totalQuestions);
   }
 
   @Get('analytics')
   getAnalytics(@Request() req) {
-    return this.salesLearningService.getAnalytics(req.user.userId);
+    return this.salesLearningService.getAnalytics(req.user.id);
   }
 }
